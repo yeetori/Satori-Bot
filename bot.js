e643bd80e25e820af1344786bf7ca41ccae9fae2
yeetori <fedgar09@hotmail.com>
2021-09-10 02:38:41 -0600

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,26 @@
+const Eris = require("eris");
+const token = require("./token.json")
+
+const bot = new Eris(token.token);
+bot.on("ready", () => {
+    console.log("Ready!");
+});
+
+bot.on("messageCreate", (msg) => {
+    var keyword = "!"
+
+    if (msg.content.toLowerCase() === keyword + "ping") {
+        bot.createMessage(msg.channel.id, "Pong!");
+    }
+    switch (msg.content.toLowerCase()){
+        case keyword + "ping":
+            bot.createMessage(msg.channel.id, "Pong!");
+            break;
+        case keyword + "pong":
+            bot.createMessage(msg.channel.id, "Ping!");
+            break;
+        case keyword + "avatar":
+            bot.createMessage(msg.channel.id, user.id.avatarURL)
+    }
+});
+bot.connect();
