f540f36d85c252e2decfe633876c6acc3e8d6a37
yeetori <fedgar09@hotmail.com>
2021-09-10 03:17:12 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -19,8 +19,9 @@ bot.on("messageCreate", (msg) => {
         case keyword + "pong":
             bot.createMessage(msg.channel.id, "Ping!");
             break;
-        case keyword + "avatar":
+        /*case keyword + "avatar":
             bot.createMessage(msg.channel.id, user.id.avatarURL)
+            break;*/
     }
 });
 bot.connect();
